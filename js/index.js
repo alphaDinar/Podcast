97283b48adf9a214564e5ad6356189810ade545d
cathsuchurch <cathsuchurch@gmail.com>
2023-07-17 17:27:45 +0000

    change

--- js/index.js
+++ js/index.js
@@ -75,3 +75,37 @@ const togglePlay = (el) => {
     })
   }
 }  
+
+document.querySelectorAll('.head_box .mid span small').forEach((el, i) => {
+  anime({
+    loop: true,
+    targets: el,
+    scale: [0, 1],
+    opacity: [0.5, 1],
+    duration: 2000,
+    delay: 50 * i,
+    width: [20, 30, 20, 25, 30],
+  })
+})
+
+document.querySelectorAll('.head_box .left nav i').forEach((el, i) => {
+  anime({
+    targets: el,
+    opacity: [0, 1],
+    translateX: [-30, 0]
+  })
+})
+
+anime({
+  loop: true,
+  duration: 3000,
+  targets: '#clock',
+  rotate: [0, 360],
+  opacity: [0, 1]
+})
+
+anime({
+  targets: '.head_box .mid div',
+  scale: [0.8, 1],
+  duration: 300,
+})
