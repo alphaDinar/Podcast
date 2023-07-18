4c784ee252dded7a1f68ce254066234da5d244b8
cathsuchurch <cathsuchurch@gmail.com>
2023-07-18 17:37:45 +0000

    chane

---
+++ js/dashboard.js
@@ -0,0 +1,36 @@
+const sideBar = document.querySelector('.sideBar')
+const main = document.querySelector('.main')
+
+const toggleSideBar = () => {
+  sideBar.classList.toggle('change')
+  main.classList.toggle('change')
+}
+
+document.querySelectorAll('.stat').forEach((el,i)=>{
+  anime({
+    targets : el,
+    translateY : [40,0],
+    duration : 1000,
+    opacity : [0,1],
+    delay : 100 * i
+  })
+})
+
+
+document.querySelector('.sideBar').innerHTML += `
+<button class="material-symbols-outlined" onclick="toggleSideBar()">linear_scale</button>
+<header>
+  <img src="images/logo.png" id="logo">
+</header>
+<nav>
+  <a href="dashboard.html"> <i class="material-symbols-outlined">dashboard</i> <span>Dashboard</span> </a>
+  <a href="programs.html"> <i class="material-symbols-outlined">radio</i> <span>Programs</span> </a>
+  <a href=""> <i class="material-symbols-outlined">trophy</i> <span>Competitions</span> </a>
+  <a href=""> <i class="material-symbols-outlined">podcasts</i> <span>Podcasts</span></a>
+  <a href=""> <i class="material-symbols-outlined">tag</i> <span>Blogs</span></a>
+  <a href=""> <i class="material-symbols-outlined">news</i> <span>News</span></a>
+</nav>
+<footer>
+  <a class="material-symbols-outlined">power_settings_new</a>
+</footer>
+`
--- js/index.js
+++ js/index.js
@@ -1,58 +1,59 @@
-const runInfoAnime = () => {
-  document.querySelectorAll('.info p').forEach((el, i) => {
-    anime({
-      targets: el,
-      translateY: [-30, 0],
-      duration: 1000,
-      delay: i * 100,
-      opacity: [0, 1]
-    })
-  })
-  document.querySelectorAll('.info_holder nav i').forEach((el, i) => {
-    anime({
-      targets: el,
-      delay: 100 * i,
-      duration: 1000,
-      opacity: [0, 1],
-      translateX: [-30, 0]
-    })
-  })
-}
-runInfoAnime()
+// const runInfoAnime = () => {
+//   document.querySelectorAll('.info p').forEach((el, i) => {
+//     anime({
+//       targets: el,
+//       translateY: [-30, 0],
+//       duration: 1000,
+//       delay: i * 100,
+//       opacity: [0, 1]
+//     })
+//   })
+//   document.querySelectorAll('.info_holder nav i').forEach((el, i) => {
+//     anime({
+//       targets: el,
+//       delay: 100 * i,
+//       duration: 1000,
+//       opacity: [0, 1],
+//       translateX: [-30, 0]
+//     })
+//   })
+// }
+// runInfoAnime()
 
 const info_holder = document.querySelector('.head_box .right')
 const page = document.querySelector('.page')
 window.onload = () => {
   var val = window.innerWidth
-  fixInfoHolder(val)
-  runInfoAnime()
+  // fixInfoHolder(val)
+  // runInfoAnime()
 }
 
 window.onresize = () => {
   var val = window.innerWidth
-  fixInfoHolder(val)
+  // fixInfoHolder(val)
 }
 
 
+const top_box = document.querySelector('.top_box')
 
-const fixInfoHolder = (val) => {
-  if (val < 1001) {
-    if (!document.querySelector('.info_holder_box')) {
-      var info_holder_clone = info_holder.cloneNode(true)
-      info_holder_clone.classList.remove('right')
-      info_holder.style.display = 'none'
-      var info_holder_clone_box = document.createElement('section')
-      info_holder_clone_box.classList.add('info_holder_box')
-      info_holder_clone_box.appendChild(info_holder_clone)
-      page.appendChild(info_holder_clone_box)
-    }
-  } else {
-    if (document.querySelector('.info_holder_box')) {
-      info_holder.style.display = 'flex'
-      document.querySelector('.info_holder_box').remove()
-    }
-  }
-}
+// const fixInfoHolder = (val) => {
+//   if (val < 1001) {
+//     if (!document.querySelector('.info_holder_box')) {
+//       var info_holder_clone = info_holder.children[1].cloneNode(true)
+//       info_holder_clone.classList.remove('right')
+//       info_holder.style.display = 'none'
+//       var info_holder_clone_box = document.createElement('section')
+//       info_holder_clone_box.classList.add('info_holder_box')
+//       info_holder_clone_box.appendChild(info_holder_clone)
+//       top_box.appendChild(info_holder_clone_box)
+//     }
+//   } else {
+//     if (document.querySelector('.info_holder_box')) {
+//       info_holder.style.display = 'flex'
+//       document.querySelector('.info_holder_box').remove()
+//     }
+//   }
+// }
 
 const audio_file = document.querySelector('audio')
 const audio_progress = document.querySelector('.player_bar .progress')
